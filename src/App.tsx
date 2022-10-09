6cc96a0df4ac05f3ffe31a8d15decc33cef577c2
eduardogomesf <eduardogoomesf@gmail.com>
2022-10-09 13:19:11 -0300

    feat: add default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { GlobalStyle } from "./styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/globalStyle";
+import { defaultTheme } from "./styles/themes/default";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello world</h1>
       <GlobalStyle />
-    </>
+    </ThemeProvider>
 
   )
 }
