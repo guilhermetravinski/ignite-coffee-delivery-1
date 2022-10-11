54d42564ce3d2eca8adb4336a643cb72fb747729
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-10-11 19:23:19 -0300

    feat: add FormContainer in Cart page

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,9 +1,71 @@
-import { CartContainer } from './styles'
+import { MapPinLine } from 'phosphor-react'
+import {
+  CardHeader,
+  CartContainer,
+  CheckoutContainer,
+  DefaultInput,
+  FormContainer,
+  FullsizeInput,
+  InputsContainer,
+  SmallInput,
+  Subtitle,
+} from './styles'
 
 export function Cart() {
   return (
     <CartContainer>
-      <h1>OI CART</h1>
+      <FormContainer>
+        <Subtitle>Complete seu pedido</Subtitle>
+        <form>
+          <CardHeader>
+            <MapPinLine size={22} />
+            <div>
+              <strong>Endereço de Entrega</strong>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </CardHeader>
+          <InputsContainer>
+            <DefaultInput type="text" placeholder="CEP" />
+            <FullsizeInput type="text" placeholder="Rua" />
+            <div>
+              <DefaultInput type="text" placeholder="Número" />
+              <FullsizeInput
+                type="text"
+                placeholder="Complemento"
+                isOptional={true}
+              />
+            </div>
+            <div>
+              <DefaultInput type="text" placeholder="Bairro" />
+              <FullsizeInput type="text" placeholder="Cidade" />
+              <SmallInput type="text" placeholder="UF" />
+            </div>
+          </InputsContainer>
+        </form>
+        <footer>
+          <div>
+            <span>$ Icon</span>
+            <div>
+              <strong>Pagamento</strong>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
+            </div>
+          </div>
+          <ul>
+            <li>
+              <button>Cartão de Crédito</button>
+            </li>
+            <li>
+              <button>Cartão de Débito</button>
+            </li>
+            <li>
+              <button>Dinheiro</button>
+            </li>
+          </ul>
+        </footer>
+      </FormContainer>
+      <CheckoutContainer></CheckoutContainer>
     </CartContainer>
   )
 }
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import styled from 'styled-components'
 
 export const CartContainer = styled.main`
@@ -5,10 +6,104 @@ export const CartContainer = styled.main`
   margin: 0 auto 9.75rem;
 
   position: relative;
-  top: 120px;
+  top: 9rem;
 
   padding: 0 1rem;
 
-  > h1 {
+  display: flex;
+`
+
+export const FormContainer = styled.div`
+  width: 100%;
+  max-width: 40rem;
+
+  display: flex;
+  flex-direction: column;
+
+  > form {
+    margin-top: 0.9375rem;
+
+    background-color: ${(props) => props.theme['--base-card']};
+    border-radius: 6px;
+    padding: 2.5rem;
   }
 `
+
+export const Subtitle = styled.h2`
+  font-family: 'Baloo 2', cursive;
+  font-weight: 700;
+  font-size: 1.125rem;
+  line-height: 1.4375rem;
+
+  color: ${(props) => props.theme['--base-subtitle']};
+`
+
+export const CardHeader = styled.div`
+  display: flex;
+  gap: 0.5rem;
+
+  > svg {
+    color: ${(props) => props.theme['--brand-yellow-dark']};
+  }
+
+  > div {
+    > strong {
+      line-height: 1.3125rem;
+      font-weight: 400;
+
+      color: ${(props) => props.theme['--base-subtitle']};
+    }
+
+    > p {
+      font-size: 0.875rem;
+      line-height: 1.125rem;
+      font-weight: 400;
+
+      color: ${(props) => props.theme['--base-text']};
+    }
+  }
+`
+
+export const InputsContainer = styled.div`
+  margin-top: 2rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  > div {
+    display: flex;
+    gap: 0.75rem;
+  }
+`
+
+const BaseInput = styled.input`
+  padding: 0.75rem;
+  background: ${(props) => props.theme['--base-input']};
+  color: ${(props) => props.theme['--base-text']};
+  border: 1px solid ${(props) => props.theme['--base-button']};
+  border-radius: 4px;
+
+  ::placeholder {
+    color: ${(props) => props.theme['--base-label']};
+  }
+`
+
+export const DefaultInput = styled(BaseInput)`
+  max-width: 12.5rem;
+`
+
+type FullsizeInputProps = {
+  isOptional?: boolean
+}
+
+export const FullsizeInput = styled(BaseInput) <FullsizeInputProps>`
+  width: 100%;
+
+`
+
+export const SmallInput = styled(BaseInput)`
+  max-width: 3.75rem;
+`
+
+export const CheckoutContainer = styled.div``
