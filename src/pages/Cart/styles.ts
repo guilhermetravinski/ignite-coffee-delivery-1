4ca72f50bf2dc2518549c96d8869dceecdd91780
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-10-11 17:19:06 -0300

    feat: start Cart page

---
+++ src/pages/Cart/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const CartContainer = styled.main`
+  width: 100%;
+  margin: 0 auto 9.75rem;
+
+  position: relative;
+  top: 120px;
+
+  padding: 0 1rem;
+
+  > h1 {
+  }
+`
